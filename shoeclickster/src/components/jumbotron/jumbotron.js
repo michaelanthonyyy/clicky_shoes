e4bd324cb790f2ce75e272fa4531ea5f75345932
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-22 19:58:03 -0800

    added jumbotron to be nested under navbar

--- shoeclickster/src/components/jumbotron/jumbotron.js
+++ shoeclickster/src/components/jumbotron/jumbotron.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./jumbotron.css";
 
-
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+            <h1 className="title">Clicky Game!</h1>
+            <h2 className="description">Click on an image to earn points, but don't click on any picture more than once!</h2>
+        </div>
+    );
+}
 
 
 export default Jumbotron;
