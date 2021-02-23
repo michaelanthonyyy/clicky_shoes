daa98935193815b909edc921b98ba3818c8e85f4
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-22 20:30:13 -0800

    got button cards printed onto page under jumbotron. need to add props functions to add to score

--- shoeclickster/src/App.js
+++ shoeclickster/src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
+import Wrapper from "./components/wrapper/wrapper.js"
 import Navbar from "./components/navbar/navbar.js";
 import Jumbotron from "./components/jumbotron/jumbotron.js";
 import Card from "./components/card/card.js";
 import shoes from "./shoes.json";
-import { render } from "react-dom";
 
 class App extends Component {
     state = {
+        shoes,
         score: 0,
     }
 
     render() {
         return (
             <div>
-            <Navbar />
-            
+                {/* navbar */}
+            <Jumbotron />
+            <Wrapper>
+            {this.state.shoes.map(shoe => (
+                <Card
+                id={shoe.id}
+                name={shoe.name}
+                image={shoe.image}
+                />
+            ))}
+
+            </Wrapper>
             </div>
         )
     }
--- shoeclickster/src/components/navbar/navbar.js
+++ shoeclickster/src/components/navbar/navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./navbar.css";
 
-function Navbar(props) {
+function Navbar() {
     <div className = "navbar">
         <h2 className = "title">Clicky Game</h2>
         <h2 className = "instruct">Click an image to begin</h2>
-        <h2 className = "gameScore">Score: {props.total}</h2>
+        <h2 className = "gameScore">Score: </h2>
     </div>
 }
 
