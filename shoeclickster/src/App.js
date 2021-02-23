92d90b684be1867564b818c72e8c09497be8f420
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-22 19:06:37 -0800

    added shoes.json file

--- shoeclickster/src/App.js
+++ shoeclickster/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
+import Navbar from "./components/navbar/navbar.js";
+import Jumbotron from "./components/jumbotron/jumbotron.js";
+import Card from "./components/card/card.js";
+import shoes from "./shoes.json";
 
 
-class App extends Comonent {
-    state = {
-        shoes: shoes
-    }
+// class App extends Comp {
+//     state = {
+//         shoes: shoes
+//     }
 
 
-    render() {
-        // return (
-        //     // <Wrapper>
-        //     //     <Title>Clicky Game</Title>
-        //     //     {this.state.shoes.map(shoes => (
-        //     //         <ShoesCard
-        //     //         id={shoes.id}
-        //     //         key={shoes.id}
-        //     //         image={shoes.image}
-        //     //         />
-        //     //     ))}
-        //     // </Wrapper>
-        // );
-    }
-}
+//     render() {
+//         // return (
+//         //     // <Wrapper>
+//         //     //     <Title>Clicky Game</Title>
+//         //     //     {this.state.shoes.map(shoes => (
+//         //     //         <ShoesCard
+//         //     //         id={shoes.id}
+//         //     //         key={shoes.id}
+//         //     //         image={shoes.image}
+//         //     //         />
+//         //     //     ))}
+//         //     // </Wrapper>
+//         // );
+//     }
+// }
 
-export default App;
+// export default App;
