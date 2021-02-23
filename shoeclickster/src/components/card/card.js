9b7ca1278c18177ea3e2d69a217fab446741762e
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-22 19:15:58 -0800

    created card body

--- shoeclickster/src/components/card/card.js
+++ shoeclickster/src/components/card/card.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./card.css";
 
-
+function Card(props) {
+    return (
+        <div className="card click-item" onClick={() => props.selectCard(props.id)}>
+            <img className="shoeCard" src={props.image} />
+        </div>
+    )
+}
 
 
 export default Card;
