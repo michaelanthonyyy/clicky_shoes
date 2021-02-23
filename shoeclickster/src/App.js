825e20e3e4296fbb5f0061b08b43bffc7a40ce00
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-02-22 19:46:17 -0800

    tested app.js

--- shoeclickster/src/App.js
+++ shoeclickster/src/App.js
@@ -3,28 +3,20 @@ import Navbar from "./components/navbar/navbar.js";
 import Jumbotron from "./components/jumbotron/jumbotron.js";
 import Card from "./components/card/card.js";
 import shoes from "./shoes.json";
+import { render } from "react-dom";
 
+class App extends Component {
+    state = {
+        score: 0,
+    }
 
-// class App extends Comp {
-//     state = {
-//         shoes: shoes
-//     }
-
-
-//     render() {
-//         // return (
-//         //     // <Wrapper>
-//         //     //     <Title>Clicky Game</Title>
-//         //     //     {this.state.shoes.map(shoes => (
-//         //     //         <ShoesCard
-//         //     //         id={shoes.id}
-//         //     //         key={shoes.id}
-//         //     //         image={shoes.image}
-//         //     //         />
-//         //     //     ))}
-//         //     // </Wrapper>
-//         // );
-//     }
-// }
-
-// export default App;
+    render() {
+        return (
+            <div>
+            <Navbar />
+            
+            </div>
+        )
+    }
+};
+export default App;
--- shoeclickster/src/components/navbar/navbar.js
+++ shoeclickster/src/components/navbar/navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./navbar.css";
 
-
+function Navbar(props) {
+    <div className = "navbar">
+        <h2 className = "title">Clicky Game</h2>
+        <h2 className = "instruct">Click an image to begin</h2>
+        <h2 className = "gameScore">Score: {props.total}</h2>
+    </div>
+}
 
 
 export default Navbar;
